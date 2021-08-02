2bb0407b3fa4f15286caca30016aa378952fc564
Julie J <jeannet.julie@gmail.com>
2021-08-02 09:57:04 +0200

    afficher le détail des participants et le total en fonction de l'état d'une case à cocher

--- src/components/Pot.js
+++ src/components/Pot.js
@@ -46,15 +46,45 @@ const Pot = () => {
         setAmount(0.0);
         firstnameRef.current.focus();
     }
+    // pour l'état de la case à cocher qui permet d'afficher le total
+    const [showDetails, setShowDetails] = useState(false);
+    const [total, setTotal] = useState(0);
+    // on veut un useEffect qui aura  pour dépendance users
+    useEffect(() => {
+        // on utilise la méthode reduce qui est dans le  prototype de array
+        const total = users.reduce(
+            (acc,current)=> acc = acc+current.amount,0)
+        setTotal(total);
+    }, [users]);
+    const userSingularPlurial = users.length > 1? ("user") : ("user");
+    const toggleDetails = e => setShowDetails(!showDetails);
     return (
         <>
             <h2>Votes</h2>
+            <div>Total : {total} € ( de {users.length} {userSingularPlurial})</div>
             <form onSubmit={handleSubmit}>
                 <input type="text" value={firstname} ref={firstnameRef} placeholder="votre prénom" onChange={handleFirstname} />
                 <input type="text" value={lastname} placeholder="votre nom" onChange={handleLastname} />
                 <input type="text" value={amount} placeholder="montant ajouté" onChange={handleAmount} />
                 <button type="submit">Valider</button>
             </form>
+            
+            <label htmlFor="details">
+                <input type="checkbox" name="details" onChange={toggleDetails} />
+                Voir les détails
+            </label>
+
+            {users.length > 0 ? (
+                users.map(
+                    // user => <div key={user.id}>{user.firstname} added {user.amount}</div>
+                    user => showDetails ? (<div key={user.id}>{user.firstname} added {user.amount}</div>)
+                    :(<div key={user.id}>{user.firstname} a participé</div>)
+                )
+            ) : (<h3>
+                Aucun participant n'a pour le moment ajouté de montant au pot de départ
+            </h3>
+
+            )}
         </>
     );
 }
